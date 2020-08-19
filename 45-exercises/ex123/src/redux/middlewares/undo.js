d9b948aa09ef757593078df982711595c5be2a55
ynonp <ynonperek@gmail.com>
2020-08-19 17:14:17 +0300

    Update undo.js
    A simple refactoring

--- 45-exercises/ex123/src/redux/middlewares/undo.js
+++ 45-exercises/ex123/src/redux/middlewares/undo.js
@@ -1,24 +1,30 @@
 let pastStates = [];
 const undo = store => next => action => {
     if (  action.type === 'UNDO') {
-        // debugger
-        if ( pastStates.length === 0 ) {
-            return;
+        if (addPastState(action)) {
+          return next(action);
         }
-        const prevState = pastStates.pop();
-        action.payload = prevState;
-        console.log('pastStates', pastStates);
-        return next(action);
     }
     else {
-        // debugger
-        // save state BEFORE action changed state,
-        // so when 'UNDO' action comes returns to that state.
+        saveStateBeforeAction(store);
+        return next(action);
+    }
+}
+
+function addPastState(action) {
+    if ( pastStates.length === 0 ) {
+        return;
+    }
+    const prevState = pastStates.pop();
+    action.payload = prevState;
+    console.log('pastStates', pastStates);
+    return true;    
+}
+
+function saveStateBeforeAction(store) {
         const state = store.getState();
         pastStates.push(state);
         console.log('pastStates', pastStates);
-        return next(action);
-    }
 }
 
 export default undo;
